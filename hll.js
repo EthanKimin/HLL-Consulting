9d0cf02c110d5489b835224593c2a901111fe13e
HLLConsultingSoftDep <84056085+HLLConsultingSoftDep@users.noreply.github.com>
2021-05-28 16:10:16 +0800

    1.2.1.
    change all

--- hll.js
+++ hll.js
@@ -1,29 +1,29 @@
 var w = window.innerWidth;
 function checkWindow() {
     if(window.innerWidth>1020){
-        document.querySelector('.thema').style.display='flex';
-    }else if(document.querySelector('input').mode==='navopen'){
-        document.querySelector('.thema').style.display='flex';
+        document.querySelector('.navmenu').style.display='flex';
+    }else if(document.querySelector('.navtoggle').mode==='navopen'){
+        document.querySelector('.navmenu').style.display='flex';
     }else{
-        document.querySelector('.thema').style.display='none';
+        document.querySelector('.navmenu').style.display='none';
     }
 };
 window.onresize = checkWindow;
             
 function navOpenClose(self){
     if(self.mode==='navopen'){
-        document.querySelector('.thema').style.display='flex';
+        document.querySelector('.navmenu').style.display='flex';
     }else{
-        document.querySelector('.thema').style.display='none';
+        document.querySelector('.navmenu').style.display='none';
     }
 }
 
 var y = pageYOffset;
 function scrollChange() {
     if(pageYOffset<5){
-        document.querySelector(".introduce").style.boxShadow ='none'
+        document.querySelector("#navbar").style.boxShadow ='none'
     }else{
-        document.querySelector(".introduce").style.boxShadow ='0 2px 5px rgba(0, 0, 0, 0.25)'
+        document.querySelector("#navbar").style.boxShadow ='0 2px 5px rgba(0, 0, 0, 0.25)'
     }
 }
 window.onscroll = scrollChange;
