b18d920dacf129d61d111742d12ab81fc72b140f
Ethan Kim <rlagmlruadle@icloud.com>
2021-06-06 02:06:39 +0800

    add scroll animation effect

--- hll.js
+++ hll.js
@@ -8,7 +8,7 @@ function checkWindow() {
     }
 };
 window.onresize = checkWindow;
-            
+
 function navOpenClose(self){
     if(self.mode==='navopen'){
         document.querySelector('.navmenu').style.display='none';
@@ -29,14 +29,29 @@ function langMenuOpen(){
     }
 }
 
+function startPage(){
+    document.querySelector('#frontpage p').classList.add('fade-in');
+    document.querySelector('#frontpage h3').classList.add('fade-in');
+}
+window.onload = startPage;
+
 function scrollChange() {
     document.querySelector('#lang-menu ul').style.display='none';
     if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
         document.querySelector("#navbar").style.boxShadow ='0 2px 5px rgba(0, 0, 0, 0.25)';
         document.getElementById("myBtn").style.display ="block";
+        if(document.body.scrollTop > 500 || document.documentElement.scrollTop > 500){
+            document.querySelector('.c1').classList.add('fade-in')
+            if(document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200){
+                for(var i = 0; i < document.querySelectorAll('.content-string').length; i=i+1)
+                document.querySelectorAll('.content-string')[i].classList.add("fade-in");
+                for(var i = 0; i < document.querySelectorAll('.content').length; i=i+1)
+                document.querySelectorAll('.content')[i].classList.add("pop-up");
+            }
+        }
     }else{
         document.querySelector("#navbar").style.boxShadow ='none';
-        document.getElementById("myBtn").style.display = "none"
+        document.getElementById("myBtn").style.display = "none";
     }
 }
 
@@ -46,3 +61,4 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
